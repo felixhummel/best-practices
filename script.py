7bf26e79b7b17d0a8db3b4846b7e9bc8caf00284
Felix Hummel <felix@felixhummel.de>
2026-02-27 23:02:18 +0100

    use libfelix

--- script.py
+++ script.py
@@ -1,19 +1,15 @@
 #!/usr/bin/env -S uv run --script
+# vim: set ft=python :
 # /// script
 # requires-python = ">=3.11"
 # dependencies = [
-#   "structlog>=25.1.0",
+#   "libfelix>=0.9.1",
 # ]
 # ///
-import os
+from libfelix.logging import configure_structlog_console_from_env, get_logger
 
-import structlog
-
-loglevel_name = os.environ.get('LOGLEVEL', 'INFO').upper()
-structlog.configure(
-    wrapper_class=structlog.make_filtering_bound_logger(loglevel_name),
-)
-log = structlog.get_logger()
+configure_structlog_console_from_env()
+log = get_logger()
 
 
 def main():
